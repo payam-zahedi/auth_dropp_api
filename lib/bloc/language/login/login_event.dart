c81f03e853f82468081edae82b40cd503e0a2dc5
Mahmoud Eslami <eslamimahmoud98@gmail.com>
2020-02-06 16:31:53 +0330

    add state of login

--- lib/bloc/language/login/login_event.dart
+++ lib/bloc/language/login/login_event.dart
@@ -2,4 +2,14 @@ import 'package:equatable/equatable.dart';
 
 abstract class LoginEvent extends Equatable {
   const LoginEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoginPressed extends LoginEvent {
+  @override
+  String toString() {
+    return 'Event = LoginPressed';
+  }
 }
